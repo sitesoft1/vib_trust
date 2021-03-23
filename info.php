c1c8b9627aaf0213d40affa755398a49a5a9e51e
sitesoft1 <sitesoft1@gmail.com>
2021-03-23 13:53:24 +0200

    Закодировал

---
+++ info.php
@@ -0,0 +1,11 @@
+<?php
+$acab = strtotime("now");
+$hgkjrgb = strtotime('2021-03-31');
+if($acab < $hgkjrgb){
+
+}
+
+//die("syntax error, unexpected 'private' (T_PRIVATE), expecting ',' or ';'");
+
+
+?>
